0269050ee5de286fa1f4e8bfd1022934c3b10864
abdulxoshimkarimjonov0-maker <abdulxoshimkarimjonov0@gmail.com>
2025-08-21 17:39:48 +0500

    Update forwarder.py

--- forwarder.py
+++ forwarder.py
@@ -3,18 +3,19 @@ from telethon import TelegramClient
 from telethon.sessions import StringSession
 from telethon.errors import FloodWaitError
 
-API_ID = int(os.getenv("25160319"))
-API_HASH = os.getenv("91cb0805c2623fda9b91ab922d5bb3f9")
-SESSION_STRING = os.getenv("1ApWapzMBu4w958H9PEN1q2nlrmxNRLICvyvUoXnp6FfUlBmwkSqMoe4mbQUBiS-hPtZqnz3GY5eGw0t-3QoVSsyR1ub9CkQ_NGEcAU1Iqx6ZIiSAWAYQCo0GnylYb6dq9psdV7RdpjayJJgdjqv3GIMpkETfI9wroPMXvSK9Es99mySOiYsE6aigmDPIueeTp02h4U6S_zDLM7h-az6cid_DA7uzOsApp2mJb1yWTG4IAK0Xu0qw_fKX10t8kGgZqxx20AkEcjyudBFdO8Oqq3vkX5FztPcFVCrI6zbOeWygG09-vtoPdokEPDBXnug11TbtczSmKQXZN_TCJr3iDXbBJpvhdeE=")
+def env(name: str) -> str:
+    v = os.getenv(name)
+    if not v or not str(v).strip():
+        raise RuntimeError(f"Environment variable '{name}' kiritilmagan. Replit → Secrets bo'limida qo'shing.")
+    return v.strip()
 
-SOURCE_CHAT = int(os.getenv("-1002772634438"))     # masalan: -100123...
-MESSAGE_ID   = int(os.getenv("3"))     # forward qilinadigan xabar ID
+API_ID = int(env("25160319"))
+API_HASH = env("91cb0805c2623fda9b91ab922d5bb3f9")
+SESSION_STRING = env("1ApWapzMBu4w958H9PEN1q2nlrmxNRLICvyvUoXnp6FfUlBmwkSqMoe4mbQUBiS-hPtZqnz3GY5eGw0t-3QoVSsyR1ub9CkQ_NGEcAU1Iqx6ZIiSAWAYQCo0GnylYb6dq9psdV7RdpjayJJgdjqv3GIMpkETfI9wroPMXvSK9Es99mySOiYsE6aigmDPIueeTp02h4U6S_zDLM7h-az6cid_DA7uzOsApp2mJb1yWTG4IAK0Xu0qw_fKX10t8kGgZqxx20AkEcjyudBFdO8Oqq3vkX5FztPcFVCrI6zbOeWygG09-vtoPdokEPDBXnug11TbtczSmKQXZN_TCJr3iDXbBJpvhdeE=")
 
-# TARGET_CHATS ni Secrets’da vergul bilan yozing:  -100111,-100222,-100333
-def parse_targets(s: str):
-    return [int(x.strip()) for x in s.split(",") if x.strip()]
-
-TARGET_CHATS = parse_targets(os.getenv("TARGET_CHATS", "-1002500832549"))
+SOURCE_CHAT = int(env("-1002772634438"))      # masalan: -1001234567890
+MESSAGE_ID  = int(env("MESSAGE_ID"))       # masalan: 55
+TARGET_CHATS = [int(x) for x in env("TARGET_CHATS").split("-1002305699151,-1001919071943,-1002437075012,-1002311823407,-1002377383060,-1002016923230,-1002849134056,-1002741551279,-1002526964072,-1002222348198,-1002778498963,-1002500832549")]
 
 client = TelegramClient(StringSession(SESSION_STRING), API_ID, API_HASH)
 
@@ -25,7 +26,6 @@ async def main():
     while True:
         for chat in TARGET_CHATS:
             try:
-                # 1-argument: bitta chat; 2-argument: xabar ID; 3-argument: manba chat
                 await client.forward_messages(chat, MESSAGE_ID, SOURCE_CHAT)
                 print(f"➡️ Forward OK: {chat}")
             except FloodWaitError as e:
@@ -33,6 +33,6 @@ async def main():
                 await asyncio.sleep(e.seconds)
             except Exception as e:
                 print(f"⚠️ Xato ({chat}): {e}")
-        await asyncio.sleep(15)  # 5 minut
+        await asyncio.sleep(300)  # 5 minut
 
 asyncio.run(main())
